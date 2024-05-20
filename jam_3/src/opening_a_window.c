cabae497c6fa0b7b4ab0406bfb5903d9ce987285
AGONGBONON Tobi Prince <tobi.agongbonon@epitech.eu>
2024-05-20 13:03:57 +0100

    Déplacement non achevé

--- jam_3/src/opening_a_window.c
+++ jam_3/src/opening_a_window.c
@@ -4,7 +4,13 @@
 ** File description:
 ** Day13
 */
-#include "../../include/jam.h"
+
+#include <SFML/Graphics.h>
+#include "../my.h"
+#include <stdio.h>
+#include <SFML/Window.h>
+#include <SFML/Audio.h>
+#include <SFML/System.h>
 
 move_player_t event_handler(sfEvent e, sfRenderWindow *w, sfVector2f sc, move_player_t p, rect_t *rectangle)
 {
@@ -22,13 +28,10 @@ affiche my_texture(move_player_t p)
 {
     affiche key;
     sfVector2f scale = {1, 0.55};
-    sfIntRect p_rect = sfSprite_getTextureRect(p.sprite);
-    p_rect.height += -400;
-    p_rect.width += -400;
-    p_rect.left += -400;
-    p_rect.top += -400;
 
-    key.texture = sfTexture_createFromFile("./src/Tobi/lab.jpg", NULL);
+    sfVector2f pos = sfRectangleShape_getPosition(p.outline);
+    sfIntRect rectangle = {pos.x, pos.y, 100, 100}; 
+    key.texture = sfTexture_createFromFile("./lab.jpg", NULL);
     key.sprite = sfSprite_create();
     sfSprite_setTexture(key.sprite, key.texture, sfTrue);
     sfSprite_setScale(key.sprite, scale);
@@ -40,41 +43,44 @@ void draw_sprite(affiche key)
     sfRenderWindow_drawSprite(key.window, key.sprite, NULL);
 }
 
-int launch_labyrinth(sfRenderWindow *window)
+int main(int ac, char **av)
 {
     sfVector2f scale = {0.4, 0.35};
     sfVector2f life_pos = {1700, 10};
     sfVideoMode m = {1920, 1080, 32};
     rect_t *rectangle = my_rectangle(rectangle);
     move_player_t p = init_move_player_t_struct(p);
-    int i = 0;
     affiche key = my_texture(p);
-
-    key.window = window;
+    int i = 0;
+    sfCircleShape *circle = sfCircleShape_create();
+    sfCircleShape_setOutlineColor(circle, sfBlack);
+    sfCircleShape_setRadius(circle, 120);
+    //sfCircleShape_setFillColor(circle, sfTransparent);
+    sfCircleShape_setOutlineThickness(circle, 1.6);
+    sfVector2f posi;
+    key.window = sfRenderWindow_create(m, "MH", sfDefaultStyle, NULL);
     sfRenderWindow_setFramerateLimit(key.window, 700);
     while (sfRenderWindow_isOpen(key.window)){
         p = event_handler(key.event, key.window, scale, p, rectangle);
         sfRenderWindow_clear(key.window, sfBlack);
         draw_sprite(key);
+        sfVector2f pos = sfSprite_getPosition(p.sprite);
+        posi = sfSprite_getPosition(p.sprite);
+        //sfCircleShape_setPosition(circle, posi);
+        //sfCircleShape_setTexture(circle, key.texture, sfFalse);
+        sfIntRect rectang = {posi.x, posi.y, 350, 350};
+        //sfIntRect rectang = {pos.x - 50, pos.y + 675, 215, 385};
+        //sfCircleShape_setTextureRect(circle, rectang);
+        //sfRenderWindow_drawCircleShape(key.window, circle, NULL);
+        sfSprite_setPosition(key.sprite, pos);
+        sfSprite_setTextureRect(key.sprite, rectang);
         i = 0;
         while (rectangle[i].rect != NULL){
             sfRenderWindow_drawRectangleShape(key.window, rectangle[i].rect, NULL);
             i++;
         }
-        sfIntRect p_rect = sfSprite_getTextureRect(p.sprite);
-        p_rect.height += -400;
-        p_rect.width += -400;
-        p_rect.left += -400;
-        p_rect.top += -400;
-        sfVector2f pos = {500 , 0};
-        sfSprite_setPosition(p.sprite, pos);
-        //sfSprite_setTextureRect(p.sprite, p_rect);
         set_and_draw_player(p, key.window);
         sfRenderWindow_display(key.window);
-        //sfTexture_s
     }
-    for (i = 0; rectangle[i].rect != NULL; i++)
-        sfRectangleShape_destroy(rectangle[i].rect);
-        sfSprite_destroy(key.sprite);
-        destroy_player_val(p);
+    destroy_player_val(p);
 }
